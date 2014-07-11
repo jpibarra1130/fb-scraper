dde7e99428e5adb04ef63c73435103c500992de5
Paolo Ibarra <jpibarra1130@gmail.com>
2014-07-11 12:41:58 +0800

    Used flags for passing in the params for the graph search.

--- scraper.go
+++ scraper.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	fb "github.com/huandu/facebook"
 	"log"
 )
@@ -26,10 +27,23 @@ type Category struct {
 }
 
 func getPages() []Page {
+	// fb access token
+	var accessToken = flag.String("accessToken", "", "fb access token")
+
+	// fb element e.g. page, place
+	var fbType = flag.String("fbType", "", "fb element")
+
+	// query e.g. night,philippines
+	var query = flag.String("query", "", "query")
+
+	flag.Parse()
+
+	log.Printf("fbType=%s, query=%q", *fbType, *query)
+
 	res, _ := fb.Get("/search", fb.Params{
-		"access_token": "",
-		"type":         "page",
-		"q":            "nightlife,singapore",
+		"access_token": *accessToken,
+		"type":         *fbType,
+		"q":            *query,
 	})
 
 	var items []Page
